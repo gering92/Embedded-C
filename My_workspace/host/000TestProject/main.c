12655778947a784e77615b83a38fe9cbe9cba6a1
Gerin George <geringeorge92@gmail.com>
2024-09-19 16:09:09 -0700

    Added some test projects to see how to build STM32 projects. Also added STM32F407G-DISC1 User Manual and Datasheet to repo.

---
+++ My_workspace/host/000TestProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 19, 2024
+ *      Author: ggeorge
+ */
+
+
+#include<stdio.h>
+
+int main (void) {
+	printf("Hello world\n");
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
